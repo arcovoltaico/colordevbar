8fdca604011604d766dcf190a2e4aec0d5f01aef
Jorge <info@jorgebenitezlopez.com>
2015-09-03 13:47:50 +0200

    Fixed code, less and js

--- colordevbar.js
+++ colordevbar.js
@@ -1,18 +1,22 @@
-
-$('#bdevbar').on('click', function (e) {
-    e.preventDefault();
-    $('#devbar').toggleClass('hidden');
-    if ($("#devbar").hasClass("hidden")) {
-        $("#normal3")[0].beginElement();
-        $("#normal4")[0].beginElement();
-        $("#normal5")[0].beginElement();
-        $("#normal2")[0].beginElement();
-        $("#normal")[0].beginElement();
-    } else {
-        $("#red3")[0].beginElement();
-        $("#red4")[0].beginElement();
-        $("#red5")[0].beginElement();
-        $("#red2")[0].beginElement();
-        $("#red")[0].beginElement();
-    }
+$ (document).ready(function(){ 
+        $('#bdevbar').on('click', function (e) {
+        e.preventDefault();
+        $('#devbar').toggleClass('hidden');
+        if ($("#devbar").hasClass("hidden")) {
+            for (i=1; i<6;i++) {
+                $("#normal"+i)[0].beginElement();
+            }
+        } else {
+            for (i=1; i<6;i++) {
+                $("#red"+i)[0].beginElement();
+            }
+        }
+       });
 });
+
+
+
+
+
+
+
